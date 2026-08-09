8686f4588cb98a59230ca8f7653c55f03f4dc1b5
raefhandibudiman-cmyk <raefhandibudiman@gmail.com>
2026-08-09 10:45:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,24 +12,5 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=5000,
-        debug=False,
-        use_reloader=False
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=False,
-        use_reloader=False
->>>>>>> 59e1b953e6f6057f4b3c42f6cd0105db152519e5
+        debug=False
     )
